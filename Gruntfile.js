3b1a2607be37a889beade349e181efe2845f6b6d
Chanthou Nim <chanthou@web-essentials.asia>
2014-03-28 09:23:35 +0700

    [ADD] bower grunt sass and compass script

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.file.defaultEncoding = 'utf-8';
+
+    // Project configuration.
+    grunt.initConfig({
+        dirs: {
+            bower: 'Resources/Public/BowerComponents',
+            js: {
+                src: 'Resources/Public/Javascript/Compiled',
+                dest: 'Resources/Public/Javascript'
+            },
+            coffee: {
+                src: 'Resources/Private/Coffeescript',
+                dest: 'Resources/Public/Javascript/Compiled'
+            },
+            sass: {
+                src: 'Resources/Private/Styles',
+                dest: 'Resources/Public/Css'
+            }
+        },
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                src: [
+                    '<%= dirs.js.src %>/*.js'
+                ],
+                dest: '<%= dirs.js.dest %>/app.min.js',
+                options: {
+                    sourceMap: '<%= dirs.js.dest %>/app.min.map',
+                    sourceMappingURL: 'app.min.map',
+                    sourceMapPrefix: 3
+//                    compress: {
+//                        drop_console: true
+//                    }
+                }
+            }
+        },
+        copy: {
+//            main: {
+//                flatten: true,
+//                expand: true,
+//                src: '<%= dirs.bower %>/magnific-popup/dist/magnific-popup.css',
+//                dest: '<%= dirs.sass.dest %>/'
+//            }
+        },
+        concat: {
+            dist: {
+                src: [
+                    '<%= dirs.bower %>/jquery/jquery.min.js'
+                ],
+                dest: '<%= dirs.js.dest %>/libs.min.js'
+            }
+        },
+        coffee: {
+            compile: {
+                options: {
+                    bare: false,
+                    sourceMap: true
+                },
+                files: {
+                    '<%= dirs.coffee.dest %>/cc.management.ui.js': '<%= dirs.coffee.src %>/**/*.coffee'
+                }
+            }
+        },
+        watch: {
+            coffee: {
+                files: ['<%= dirs.coffee.src %>/**/*.coffee'],
+                tasks: ['coffee', 'uglify']
+            },
+            sass: {
+                files: ['<%= dirs.sass.src %>/**/*.scss'],
+                tasks: 'compass'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "concat" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Load the plugin that provides the "watch" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Load the plugin that provides the "compass" task.
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // Load the plugin that provides the "coffee" task.
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+
+    // Load the plugin that provides the "copy" task.
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task.
+    grunt.registerTask('default', ['watch']);
+
+    // Build task.
+    grunt.registerTask('build', ['compass', 'coffee', 'uglify', 'concat']);
+}
---
+++ Resources/Public/Javascript/Compiled/cc.management.ui.js
@@ -0,0 +1,8 @@
+(function() {
+  (function($) {
+    return console.log('hello world');
+  })(jQuery);
+
+}).call(this);
+
+//# sourceMappingURL=cc.management.ui.js.map
