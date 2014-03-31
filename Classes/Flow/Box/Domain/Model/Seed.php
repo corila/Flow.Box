a26fe28c97e3c2ff30544daa055ae554feab0932
Chantheoun Kimthy <chantheoun@web-essentials.asia>
2014-03-31 15:11:43 +0700

    generate new datamodel named Seed

---
+++ Classes/Flow/Box/Domain/Model/Seed.php
@@ -0,0 +1,39 @@
+<?php
+namespace Flow\Box\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Flow.Box".              *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @Flow\Entity
+ */
+class Seed {
+
+	/**
+	 * @var string
+	 */
+	protected $name;
+
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 * @return void
+	 */
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+}
+?>
